d32d154fe446311f829063f0c5286aa1722a0aa5
yogeshwithai <kashyapyogesh10844@gmail.com>
2026-07-14 10:21:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-console.log("Welcome to Yogesh's Portfolio!");
+const text = [
+    "Student",
+    "AI Learner",
+    "Web Developer",
+    "Future Software Engineer"
+];
 
-document.querySelectorAll("nav a").forEach(link => {
-    link.addEventListener("click", function(){
-        console.log("Clicked: " + this.innerText);
-    });
-});
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+
+    if (count === text.length) {
+        count = 0;
+    }
+
+    currentText = text[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector(".typing-text span").textContent = letter;
+
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+        setTimeout(type, 1200);
+    } else {
+        setTimeout(type, 100);
+    }
+
+})();
